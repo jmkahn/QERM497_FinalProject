37c45121d9ed1a4a274d9422dfa965f1764df80a
Helen Miller <helen@ohana.org>
2023-02-16 17:42:21 -0800

    Add fire_season function

--- simulation.py
+++ simulation.py
@@ -82,23 +82,86 @@ def grow_season(world, params): #TODO: J
     pass
 
 
-def fire_season(world, params): #TODO: H
+def fire_season(forest, L, params): 
     '''fire_season runs one iteration of the world, in which wildfires initiate, propagate, and die out'''
+    # TODO: Make sure there is no bias in the order we iterate through fire cells because
+    # we are not descretizing time steps. 
+
+    # -----INITIALIZE----- #
+    # get length of non-forest border
+    d=int((forest.shape[0] - L)/2)
     # Create two lists: 
     # Fire_location = list of locations of currently on-fire cells in grid
-    # Fire_type = “T” or “G” depending on whether each cell is tree or grass fire (aligned with Fire_location list) 
+    fire_location=[]
+    # Fire_type = 1 or 2 depending on whether each cell is tree or grass fire (aligned with Fire_location list) 
+    fire_type=[]
+
+    # -----FIRE SEASON STARTS----- #
+    ### IGNITION
     # Randomly select some of the vegetation cells to ignite and add to the lists 
+    ignition_probs = np.random.rand(L*L)
+    ignition_probs.resize(L,L)
+    grass_ignitions = (ignition_probs < params["p_i_g"]) & (forest[d:L+d, d:L+d] == 1)
+    tree_ignitions = (ignition_probs < params["p_i_t"]) & (forest[d:L+d, d:L+d] == 2)
+    
+    # add grass to fire_location list
+    # need to add d back in so it lines up with full world with boundaries
+    grass_fire_locations = np.where(grass_ignitions) 
+    for i, grass_x in enumerate(grass_fire_locations[0]): 
+        # Need to add d back in
+        fire_location.append((grass_x + d, grass_fire_locations[1][i] + d))
+        fire_type.append(1)
+    # Add tree to fire_location list
+    tree_fire_locations = np.where(tree_ignitions)
+    for i, tree_x in enumerate(tree_fire_locations[0]): 
+        fire_location.append((tree_x + d, tree_fire_locations[1][i] + d))
+        fire_type.append(2)
+
+    ### FIRE SPREADS UNTIL IT IS ALL BURNED OUT
     # While there are still cells on fire:
-    # Take the first element in fire_location: 	
-    # Get its neighbors (3x3 moore neighborhood)
-    # Randomly select which of its neighbors get set on fire (probabilities depend on moisture and focal/neighbor state)
-    # Add location of each neighbor which gets set on fire gets to end of fire_location list
-    # Add type of each neighbor which gets set on fire to fire_type end of list
-    # Update world
-    # Change focal cell to ash
-    # Change appropriate neighbors to fire
-    # Remove first element from fire_location list
-    # Remove first element from fire_type list
-    # (Optionally record data)
+    while(len(fire_location) > 0):
+
+        # Take the first element in fire_location: 	
+        location = fire_location.pop(0)
+        type = fire_type.pop(0)
+        # set it to ash
+        forest[location] = 0
+
+        # Get its neighbors (3x3 moore neighborhood)
+        neighbors = forest[location[0]-1:location[0]+2, location[1]-1:location[1]+2]
+        # Randomly select which of its neighbors get set on fire (probabilities depend on moisture and focal/neighbor state)
+        
+        # Get which cells fire spreads to
+        # two dimensions, one for spread, one for catch
+
+        # get non-ash neighbors
+        foliage_indices = np.nonzero(neighbors)
+        foliage_neighbors = neighbors[foliage_indices]
+        fire_probs = np.random.rand(2, len(foliage_neighbors))
+
+        if type == 1: # GRASS
+            spread = fire_probs[0] < params["r_g_spread"]
+        else: # TREE
+            spread = fire_probs[0] < params["r_t_spread"]
+        
+        # Get which cells fire actually ignites, given it spreads
+        for i, neighbor in enumerate(foliage_neighbors): 
+            if spread[i]: 
+                if ( neighbor == 1 # GRASS
+                    & (fire_probs[1][i] < params["r_g_catch"]) # grass catch prob
+                ) or ( neighbor == 2 # TREE
+                      & (fire_probs[1][i] < params["r_t_catch"]) # tree catch prob
+                ): 
+                    # Get index
+                    nx = foliage_indices[0][i]
+                    ny = foliage_indices[1][i]
+                    new_location = (location[0] + (nx-1), location[1] + (ny-1))
+                    # Add to fire list
+                    fire_location.append(new_location)
+                    # add to type list
+                    fire_type.append(neighbor)
+                    # change to ash in world
+
+        # (Optionally record data)
     # Return world at next time step (and optional data) 
-    pass
+    return forest
