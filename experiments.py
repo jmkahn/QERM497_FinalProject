420ae223a7fbb387d4fbd7ee599a8f4edafa87a7
Helen Miller <helen@ohana.org>
2023-02-24 17:38:13 -0800

    Run some experiments! See doc for outputs

---
+++ experiments.py
@@ -0,0 +1,169 @@
+# Note: this can be exported as jupyter notebook by
+# Ctl+Shift+P > Jupyter: Export Current Python File as Jupyter Notebook
+
+# %%
+from simulation import *
+from viz import plot_slices_of_simulation, veg_fire_over_time
+import matplotlib.pyplot as plt
+
+# %%
+# results_dict = run_simulation(m=0.4, L=50, t_steps=50, d=2, init_grass=0.1, init_tree=0.1, p_gro_gmax = 0.02, p_lightning=0.01, r_spr_tmax=1, r_spr_gmax=0.8, r_cat_tmax=0.4, r_cat_gmax=1, output_times=[0, 10, 20, 30, 49])
+# plot_slices_of_simulation(results_dict["output_slices"] , [0,1,2,3,4], burn_masks=results_dict["burned_area_masks"])
+
+# results_dict = run_simulation(m=0.2, L=50, t_steps=50, d=2, init_grass=0.1, init_tree=0.1, p_gro_gmax = 0.02, p_lightning=0.01, r_spr_tmax=1, r_spr_gmax=0.8, r_cat_tmax=0.4, r_cat_gmax=1, output_times=[0, 10, 20, 30, 49])
+# plot_slices_of_simulation(results_dict["output_slices"] , [0,1,2,3,4], burn_masks=results_dict["burned_area_masks"])
+
+# results_dict = run_simulation(m=0.1, L=50, t_steps=50, d=2, init_grass=0.1, init_tree=0.1, p_gro_gmax = 0.02, p_lightning=0.01, r_spr_tmax=1, r_spr_gmax=0.8, r_cat_tmax=0.4, r_cat_gmax=1, output_times=[0, 10, 20, 30, 49])
+# plot_slices_of_simulation(results_dict["output_slices"] , [0,1,2,3,4], burn_masks=results_dict["burned_area_masks"])
+
+# %% [Markdown]
+
+
+# %%
+# Initialize parameters
+p_disp = 0.01   # p Grass grows in random ash cell (long-range dispersal)
+# OR random grass cell becomes tree
+p_gro_ag = 0.3  # p grass propogates to neighboring ash cell
+# p tree propogates to neighboring cell (neighboring ash cells become grass)
+p_gro_gt = 0.2
+p_ig_g = 0.001  # p any given grass cell ignites randomly at beginning of season
+p_ig_t = 0.0005  # p any given tree cell ignites randomly at beginning of season
+p_spr_gg = 0.25  # p grass fire spreads to neighbor grass
+p_spr_tg = 0.25  # p tree fire spreads to neighbor grass
+p_spr_gt = 0.25  # p grass fire spreads to neighbor tree
+p_spr_tt = 0.25  # p tree fire spreads to neighbor tree
+
+L = 50
+time_steps = 100
+output_times = [0,1,2,30,49]
+
+# %%
+results_dict = run_simulation(m=0.5, # not currently used
+                              L=L,
+                              t_steps=time_steps,
+                              d=2,
+                              init_grass=0.1,
+                              init_tree=0.1,
+                              p_disp=p_disp,
+                              p_gro_ag=p_gro_ag,
+                              p_gro_gt=p_gro_gt,
+                              p_ig_g=p_ig_g,
+                              p_ig_t=p_ig_t,
+                              p_spr_gg=p_spr_gg,
+                              p_spr_tg=p_spr_tg,
+                              p_spr_gt=p_spr_gt,
+                              p_spr_tt=p_spr_tt,
+                              output_times=output_times)
+
+fig, ax = plot_slices_of_simulation(results_dict)
+fig.savefig("plots/defaults_maps.png")
+fig.show()
+
+fig = veg_fire_over_time(time_steps, results_dict)
+fig.savefig("plots/defaults_timeseries.png")
+
+
+
+# %% [markdown] 
+# Now the only thing to do, I think, is to start changing variables one at a time 
+# and save the outputs, and see if we can come up with some interesting dynamics, 
+# and get an idea of how parameters interact. 
+
+# %%
+# 
+# For grass-grass fire:  
+for r in np.linspace(0, 1, 11): 
+    results_dict = run_simulation(m=0.5, # not currently used
+                              L=50,
+                              t_steps=time_steps,
+                              d=2,
+                              init_grass=0.1,
+                              init_tree=0.1,
+                              p_disp=p_disp,
+                              p_gro_ag=p_gro_ag,
+                              p_gro_gt=p_gro_gt,
+                              p_ig_g=p_ig_g,
+                              p_ig_t=p_ig_t,
+                              p_spr_gg=r, # TESTING THIS ONE
+                              p_spr_tg=p_spr_tg,
+                              p_spr_gt=p_spr_gt,
+                              p_spr_tt=p_spr_tt,
+                              output_times=output_times)
+    r_txt = str(round(r, 5))
+    fig, ax = plot_slices_of_simulation(results_dict, title="p_spr_gg="+r_txt)
+    fig.savefig("plots/p_spr_gg-"+r_txt+"-maps.png")
+    fig = veg_fire_over_time(time_steps, results_dict, title="p_spr_gg="+r_txt)
+    fig.savefig("plots/p_spr_gg-"+r_txt+"-timeseries.png")
+
+# tree - tree fire
+for r in np.linspace(0, 1, 11): 
+    results_dict = run_simulation(m=0.5, # not currently used
+                              L=50,
+                              t_steps=time_steps,
+                              d=2,
+                              init_grass=0.1,
+                              init_tree=0.1,
+                              p_disp=p_disp,
+                              p_gro_ag=p_gro_ag,
+                              p_gro_gt=p_gro_gt,
+                              p_ig_g=p_ig_g,
+                              p_ig_t=p_ig_t,
+                              p_spr_gg=p_spr_gg,
+                              p_spr_tg=p_spr_tg,
+                              p_spr_gt=p_spr_gt,
+                              p_spr_tt=r, # TESTING THIS ONE
+                              output_times=output_times)
+    r_txt = str(round(r, 5))
+    fig, ax = plot_slices_of_simulation(results_dict, title="p_spr_tt="+r_txt)
+    fig.savefig("plots/p_spr_tt-"+r_txt+"-maps.png")
+    fig = veg_fire_over_time(time_steps, results_dict, title="p_spr_tt="+r_txt)
+    fig.savefig("plots/p_spr_tt-"+r_txt+"-timeseries.png")
+
+
+# tree - grass fire
+for r in np.linspace(0, 1, 11): 
+    results_dict = run_simulation(m=0.5, # not currently used
+                              L=50,
+                              t_steps=time_steps,
+                              d=2,
+                              init_grass=0.1,
+                              init_tree=0.1,
+                              p_disp=p_disp,
+                              p_gro_ag=p_gro_ag,
+                              p_gro_gt=p_gro_gt,
+                              p_ig_g=p_ig_g,
+                              p_ig_t=p_ig_t,
+                              p_spr_gg=p_spr_gg,
+                              p_spr_tg=r, # TESTING THIS ONE
+                              p_spr_gt=p_spr_gt,
+                              p_spr_tt=p_spr_tt,
+                              output_times=output_times)
+    r_txt = str(round(r, 5))
+    fig, ax = plot_slices_of_simulation(results_dict, title="p_spr_tg="+r_txt)
+    fig.savefig("plots/p_spr_tg-"+r_txt+"-maps.png")
+    fig = veg_fire_over_time(time_steps, results_dict, title="p_spr_tg="+r_txt)
+    fig.savefig("plots/p_spr_tg-"+r_txt+"-timeseries.png")
+
+# grass - tree fire 
+for r in np.linspace(0, 1, 11): 
+    results_dict = run_simulation(m=0.5, # not currently used
+                              L=50,
+                              t_steps=time_steps,
+                              d=2,
+                              init_grass=0.1,
+                              init_tree=0.1,
+                              p_disp=p_disp,
+                              p_gro_ag=p_gro_ag,
+                              p_gro_gt=p_gro_gt,
+                              p_ig_g=p_ig_g,
+                              p_ig_t=p_ig_t,
+                              p_spr_gg=p_spr_gg,
+                              p_spr_tg=p_spr_tg,
+                              p_spr_gt=r,# TESTING THIS ONE
+                              p_spr_tt=p_spr_tt,
+                              output_times=output_times)
+    r_txt = str(round(r, 5))
+    fig, ax = plot_slices_of_simulation(results_dict, title="p_spr_gt="+r_txt)
+    fig.savefig("plots/p_spr_gt-"+r_txt+"-maps.png")
+    fig = veg_fire_over_time(time_steps, results_dict, title="p_spr_gt="+r_txt)
+    fig.savefig("plots/p_spr_gt-"+r_txt+"-timeseries.png")
--- viz.py
+++ viz.py
@@ -1,6 +1,6 @@
 from matplotlib import colors
 import matplotlib.pyplot as plt
-def plot_slices_of_simulation(slices, times, burn_masks=None):
+def plot_slices_of_simulation(results_dict, title = "", plot_burn_masks=True):
     '''Params: 
         times: a list of time steps 
         slices: an array with shape (2*save_times, _, _), where L is the side length of a 2-D world. 
@@ -8,10 +8,14 @@ def plot_slices_of_simulation(slices, times, burn_masks=None):
         burn_mask (optional): array of binary masks, if included, burned areas will be displayed in orange
     Graphs the state of the simulation in a series of subplots, showing its progression over time 
     '''
-    if burn_masks is not None: 
+    slices = results_dict["output_slices"]
+    times = results_dict["output_times"]
+    if plot_burn_masks: 
         num_rows = 3
+        burn_masks = results_dict["burned_area_masks"]
     else: 
         num_rows = 2
+    
 
     colors_list = ["black", "yellow", "green", "orange"]
     cmap = colors.ListedColormap(colors_list)
@@ -29,7 +33,7 @@ def plot_slices_of_simulation(slices, times, burn_masks=None):
         ax[0][i].imshow(slices[2*i], cmap=cmap, norm=norm)
 
         # add burn_mask to during fire plots
-        if (burn_masks is not None): 
+        if (plot_burn_masks): 
             with_mask = slices[2*i+1] + 4*burn_masks[i]# 4 is the color that will be orange 
             ax[1][i].imshow(with_mask, cmap, norm)
             ax[1][i].xaxis.set_visible(False) 
@@ -48,17 +52,22 @@ def plot_slices_of_simulation(slices, times, burn_masks=None):
     #only show axis ticks for first plot
     ax[0][0].xaxis.set_visible(True) 
     ax[0][0].yaxis.set_visible(True)
-
-    fig.suptitle("State of simulation at various time steps")
+    if len(title) > 0: 
+        plt.suptitle(title) 
+    else: 
+        fig.suptitle("State of simulation at various time steps")
     return fig, ax
 
-def veg_fire_over_time(t_steps, results_dict):
+def veg_fire_over_time(t_steps, results_dict, title = ""):
     '''line plot of amount of grass, trees, and fire over time'''
     fig = plt.figure(figsize=(12, 6))
     plt.plot(range(t_steps), results_dict['grass_count'], color="yellow")
     plt.plot(range(t_steps), results_dict['tree_count'], color='green')
     plt.plot(range(t_steps), results_dict['area_burned'], color='orange')
-    plt.suptitle("Dynamics of simulation over time")
+    if len(title) > 0: 
+        plt.suptitle(title) 
+    else: 
+        plt.suptitle("Dynamics of simulation over time")
     plt.legend(fontsize="large")
     plt.xlabel("Time steps ('years')")
     plt.ylabel("Units of Vegetation/Fire")
